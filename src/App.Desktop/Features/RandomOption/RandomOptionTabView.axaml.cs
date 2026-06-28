d980d683002597b177a3eaa7ea6273c5ac1de441
YoSiem <patryk.olejnik655@gmail.com>
2026-06-28 05:01:15 +0200

    feat(random-option): add Random Option tab (view model + view)

---
+++ src/App.Desktop/Features/RandomOption/RandomOptionTabView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace App.Desktop.Features.RandomOption;
+
+public partial class RandomOptionTabView : UserControl
+{
+    public RandomOptionTabView()
+    {
+        InitializeComponent();
+    }
+}
